2988d1851423165802f91d4f9fd2cef7336f8d59
Amy <917634995@qq.com>
2022-06-09 13:55:08 +0800

    新增NC32 求平方根

---
+++ NC32/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NC32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.sqrt(2));
+            Console.WriteLine(solution.sqrt(2143195649));
+            Console.ReadLine();
+        }
+    }
+    
+    internal class Solution
+    {
+        public int sqrt(int x)
+        {
+            return (int)Math.Floor(Math.Sqrt(x));
+        }
+    }
+}
