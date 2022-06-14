9acf8836d1a13b2f08cabee5f317ec760bdda302
Amy <917634995@qq.com>
2022-06-14 18:46:00 +0800

    ADD NC76

---
+++ NC76/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NC76
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");            
+        }
+
+        class Solution
+        {
+            Stack<int> stack1 = new Stack<int>();
+            Stack<int> stack2 = new Stack<int>();
+            public void push(int node)
+            {
+                stack1.Push(node);
+            }
+            public int pop()
+            {
+                while (stack1.Count > 0)
+                {
+                    stack2.Push(stack1.Pop());
+                }
+                var result = stack2.Pop();
+                while (stack2.Count > 0)
+                {
+                    stack1.Push(stack2.Pop());
+                }
+                return result;
+            }
+        }
+    }
+}
