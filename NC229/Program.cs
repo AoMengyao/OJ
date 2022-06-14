3057a1b89cc0e65dfeaf3ee61260e951e0a76c47
Amy <917634995@qq.com>
2022-06-14 13:24:05 +0800

    ADD NC229

---
+++ NC229/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NC229
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().isUnique("nowcoder"));
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Solution
+    {
+        /**
+         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+         *
+         * 
+         * @param str string字符串 
+         * @return bool布尔型
+         */
+        public bool isUnique(string str)
+        {
+            // write code here
+            HashSet<char> set = new HashSet<char>(str.ToCharArray());
+            return str.Length == set.Count;
+        }
+    }
+}
