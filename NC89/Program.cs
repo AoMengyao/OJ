9808bee53d451b5465acad06b09961d43fce3176
Amy <917634995@qq.com>
2022-06-16 18:53:33 +0800

    NC89 add

---
+++ NC89/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NC89
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            var result=solution.trans("This is a sample", 16);
+            Console.WriteLine(result);
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Solution
+    {
+        public string trans(string s, int n)
+        {
+            // write code here
+            var list = s.Split(" ");
+            var result = new StringBuilder();
+            for (int i = list.Length - 1; i >= 0; i--)
+            {
+                var chars = list[i].ToCharArray();
+                for (int j = chars.Length - 1; j >= 0; j--)
+                {
+                    var ascii = Convert.ToInt16(chars[j]);
+                    chars[j] = (char)(ascii >= 97 ? ascii & 223 : ascii | 32);
+                }
+                result.Append(chars);
+                result.Append(' ');
+            }
+            return result.ToString().TrimEnd();
+        }
+    }
+}
