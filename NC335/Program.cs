2f29d5c977f7e76f32bef17f6f4f87d4055e19e0
Amy <917634995@qq.com>
2022-06-30 14:26:32 +0800

    NC335 ADD

---
+++ NC335/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NC335
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().getNumber("C"));
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Solution
+    {
+        /**
+         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+         *
+         * 
+         * @param S string字符串 
+         * @return int整型
+         */
+        public int getNumber(string S)
+        {
+            // write code here
+            char[] chars= S.ToCharArray();
+            int total = default(int);
+            int count = default(int);
+            for (int i = chars.Length - 1; i >= 0; i--)
+            {
+                total += (chars[i] - 'A' + 1) * (int)Math.Pow(26, count++);
+            }
+            return total;
+        }
+    };
+}
