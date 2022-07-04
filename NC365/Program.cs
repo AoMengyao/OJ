85e264d155e2bddd1b0df0c174ba6f2a28512a94
Amy <917634995@qq.com>
2022-07-04 18:14:50 +0800

    NC365 ADD

---
+++ NC365/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NC365
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().poweroftwo(6));
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Solution
+    {
+        /**
+         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+         *
+         * 
+         * @param n int整型 
+         * @return bool布尔型
+         */
+        public bool poweroftwo(int n)
+        {
+            // write code here
+            int mi = 1;
+            while (mi < n)
+            {
+                mi <<= 1;
+            }
+            return mi == n;
+        }
+    }
+}
