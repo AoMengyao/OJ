223ca4bd86941f7bab3f05f9f8578239a27aea4e
Amy <917634995@qq.com>
2022-10-18 12:52:28 +0800

    NC4 链表中是否有环

---
+++ NC4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NC4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int x)
+        {
+            val = x;
+        }
+    }
+
+
+    class Solution
+    {
+        /**
+         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+         *
+         *
+         * @param head ListNode类
+         * @return bool布尔型
+         */
+        public bool hasCycle(ListNode head)
+        {
+            // write code here
+            var list = new HashSet<ListNode>();
+            while (head != null && head.next != null)
+            {
+                if (list.Contains(head))
+                {
+                    return true;
+                }
+                else
+                {
+                    list.Add(head);
+                }
+                head = head.next;
+            }
+            return false;
+        }
+    }
+}
