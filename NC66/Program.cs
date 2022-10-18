fa57cf31f7e3ea35b7ad20ce80f22387712f89c8
Amy <917634995@qq.com>
2022-10-18 13:05:19 +0800

    NC66 两个链表的第一个公共结点

---
+++ NC66/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NC66
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+        }
+    }
+
+    class Solution
+    {
+        public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2)
+        {
+            // write code here
+            var list = new HashSet<ListNode>();
+            while (pHead1 != null)
+            {
+                list.Add(pHead1);
+                pHead1 = pHead1.next;
+            }
+
+            while (pHead2 != null)
+            {
+                if (list.Contains(pHead2))
+                {
+                    return pHead2;
+                }
+                else
+                {
+                    list.Add(pHead2);
+                }
+                pHead2 = pHead2.next;
+            }
+            return null;
+        }
+    }
+
+}
