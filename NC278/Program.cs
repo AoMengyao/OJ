6a8e70439af7bf0190d385bab15b86775125511b
敖梦瑶 <917634995@qq.com>
2022-10-21 13:28:29 +0800

    NC278 ADD

---
+++ NC278/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NC278
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        public ListNode deleteDuplication(ListNode pHead)
+        {
+
+            // write code here
+            if (pHead != null)
+            {
+                var next = pHead.next;
+                var list = new Dictionary<int, int> { { pHead.val, 1 } };
+
+                while (next != null)
+                {
+                    if (list.ContainsKey(next.val))
+                    {
+                        list[next.val]++;
+                    }
+                    else
+                    {
+                        list.Add(next.val, 1);
+                    }
+                    next = next.next;
+                }
+                ListNode link = null;
+                ListNode result = null;
+                foreach (var item in list.Keys)
+                {
+                    if (list[item] == 1)
+                    {
+                        if (link != null)
+                        {
+                            link.next = new ListNode(item);
+                            link = link.next;
+                        }
+                        else
+                        {
+                            link = new ListNode(item);
+                            result = link;
+                        }
+                    }
+                }
+                return result;
+            }
+            return pHead;
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+        }
+    }
+
+}
